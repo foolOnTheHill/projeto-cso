ca0aff082e01d16e417eb9e97eabff200b81e832
George H. Alvares <blackbird.fly95@gmail.com>
2015-06-22 00:41:47 -0300

    fixing some stuff

--- projeto.scala
+++ projeto.scala
@@ -29,42 +29,63 @@ object NomeEspec {
 
 	//-------------------------------- Processos
 
-	/* Adiciona estudantes à fila.
-	*  - primeiro: id do primeiro estudante a chegar.
-	*  - filaTiq: lista compartilhada representando os estudantes que estão na fila. */
-	def novosEstudantes(primeiro: Int, filaTiq: MutableList[Int]) = proc {
+	/* Adiciona estudantes à fila e os avança, sincronizando com o processo 'caixa'.
+	*  - primeiro: id do primeiro estudante a chegar. 
+	*  - comprarTiquete, sairCaixa: canais de sic. com o caixa. */
+	def filaTiquete(primeiro: Int, comprarTiquete: Seq[![Int]], sairCaixa: Seq[?[Int]]) = proc {
 		var prox: Int = primeiro
+		var filaTiq = new List() // Lista representando os estudantes na fila
+		var estudante: Int = 0 // Variavel para o estudante no começo da fila
+		var caixa: Int = 0 // Variável para o id do caixa no qual um estudante vai comprar o tíquete
+		var tiq: Int = 0 // Variavel com o id da catraca para a qual o estudante deve ir
+
+		// 'Escolha interna'
 		val seed = new Random(System.currentTimeMillis())
 		var res: Int = 0
+		
 		while (true) {
 			println("#" + prox + " chegou no RU.")
 			
 			res = seed.nextInt(2)+1
 			res match {
 				case 1 =>
+					filaTiq = filaTiq ++ [prox]
 					println("#" + prox + " entrou na fila para comprar o Tiquete")
-					filaTiq = filaTiq ++ List(prox)
 				case 2 =>
 					println("#" + prox + " desistiu da fila")
 			}
-
 			prox = proxEstudante(prox)
-		}
-	}
 
-	/* Avança os estudantes na fila sincronizando com o processo 'caixa'.
-	*  - filaTiq: lista compartilhada representando os estudantes que estão na fila do Tiquete.
-	*  - filaEnt lista compartilhada representando os estudantes que estão na fila de Entrada. 
-	*  - entrarCaixa, comprarTiquete, sairCaixa: canais de sic. com o caixa. */
-	def filaTiquete(filaTiq: MutableList[Int], filaEnt: MutableList[Int], entrarCaixa: ?[Int], comprarTiquete: ![Unit], sairCaixa: ![Unit]) = proc {
+			if (filaTiq.length > 0) {
+				estudante = filaTiq.head
+
+				print("#" + estudante + " digite o caixa em que quer comprar o tíquete: ")
+				caixa = Console.readInt()
+
+				comprarTiquete(caixa)!estudante;
+				tiq = sairCaixa? ;
+				println("#" + estudante + " comprou o tiquete para a catraca #" + tiq)
+
+				filaTiq = filaTiq.tail // Atualiza a fila
+			}
+
+		}
 	}
 
 	/* Avança os estudantes na fila sincronizando com o processo 'filaTiquete'.
 	*  - i: id do caixa.
-	*  - filaTiq: lista compartilhada representando os estudantes que estão na fila do Tiquete.
-	*  - filaEnt lista compartilhada representando os estudantes que estão na fila de Entrada. 
-	*  - entrarCaixa, comprarTiquete, sairCaixa: canais de sic. com o caixa. */
-	def caixa(i: Int, filaTiq: List[Int], entrarCaixa: ![Int], comprarTiquete: ?[Unit], sairCaixa: ?[Unit]) {
+	*  - comprarTiquete, sairCaixa: canais de sic. com o caixa. */
+	def caixa(i: Int, comprarTiquete: ?[Int], sairCaixa: ![Int]) {
+		var estudante: Int = 0
+		while (true) {
+			estudante = comprarTiquete? ;
+			println("#" + estudante + "entrou no caixa #" + i)
+
+			if (estudante%2 == 1) 
+				sairCaixa!1
+			else 
+				sairCaixa!2
+		}
 	}
 
 	def main(args: Array[String]) {
